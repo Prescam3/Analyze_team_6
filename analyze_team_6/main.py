cc844969f142f988594375fb5d6457cc409dc3fd
MoepeK <k.moepe@gmail.com>
2020-08-13 10:26:55 +0200

    PEP8
    Cleaned code to PEP8 standards of coding.

--- analyze_team_6/main.py
+++ analyze_team_6/main.py
@@ -41,35 +41,43 @@ mun_dict = {
 # dictionary of english stopwords
 stop_words_dict = {
     'stopwords':[
-        'where', 'done', 'if', 'before', 'll', 'very', 'keep', 'something', 'nothing', 'thereupon', 
-        'may', 'why', 'â€™s', 'therefore', 'you', 'with', 'towards', 'make', 'really', 'few', 'former', 
-        'during', 'mine', 'do', 'would', 'of', 'off', 'six', 'yourself', 'becoming', 'through', 
-        'seeming', 'hence', 'us', 'anywhere', 'regarding', 'whole', 'down', 'seem', 'whereas', 'to', 
-        'their', 'various', 'thereafter', 'â€˜d', 'above', 'put', 'sometime', 'moreover', 'whoever', 'although', 
-        'at', 'four', 'each', 'among', 'whatever', 'any', 'anyhow', 'herein', 'become', 'last', 'between', 'still', 
-        'was', 'almost', 'twelve', 'used', 'who', 'go', 'not', 'enough', 'well', 'â€™ve', 'might', 'see', 'whose', 
-        'everywhere', 'yourselves', 'across', 'myself', 'further', 'did', 'then', 'is', 'except', 'up', 'take', 
-        'became', 'however', 'many', 'thence', 'onto', 'â€˜m', 'my', 'own', 'must', 'wherein', 'elsewhere', 'behind', 
-        'becomes', 'alone', 'due', 'being', 'neither', 'a', 'over', 'beside', 'fifteen', 'meanwhile', 'upon', 'next', 
-        'forty', 'what', 'less', 'and', 'please', 'toward', 'about', 'below', 'hereafter', 'whether', 'yet', 'nor', 
-        'against', 'whereupon', 'top', 'first', 'three', 'show', 'per', 'five', 'two', 'ourselves', 'whenever', 
-        'get', 'thereby', 'noone', 'had', 'now', 'everyone', 'everything', 'nowhere', 'ca', 'though', 'least', 
-        'so', 'both', 'otherwise', 'whereby', 'unless', 'somewhere', 'give', 'formerly', 'â€™d', 'under', 
-        'while', 'empty', 'doing', 'besides', 'thus', 'this', 'anyone', 'its', 'after', 'bottom', 'call', 
-        'nâ€™t', 'name', 'even', 'eleven', 'by', 'from', 'when', 'or', 'anyway', 'how', 'the', 'all', 
-        'much', 'another', 'since', 'hundred', 'serious', 'â€˜ve', 'ever', 'out', 'full', 'themselves', 
-        'been', 'in', "'d", 'wherever', 'part', 'someone', 'therein', 'can', 'seemed', 'hereby', 'others', 
-        "'s", "'re", 'most', 'one', "n't", 'into', 'some', 'will', 'these', 'twenty', 'here', 'as', 'nobody', 
-        'also', 'along', 'than', 'anything', 'he', 'there', 'does', 'we', 'â€™ll', 'latterly', 'are', 'ten', 
-        'hers', 'should', 'they', 'â€˜s', 'either', 'am', 'be', 'perhaps', 'â€™re', 'only', 'namely', 'sixty', 
-        'made', "'m", 'always', 'those', 'have', 'again', 'her', 'once', 'ours', 'herself', 'else', 'has', 'nine', 
-        'more', 'sometimes', 'your', 'yours', 'that', 'around', 'his', 'indeed', 'mostly', 'cannot', 'â€˜ll', 'too', 
-        'seems', 'â€™m', 'himself', 'latter', 'whither', 'amount', 'other', 'nevertheless', 'whom', 'for', 'somehow', 
-        'beforehand', 'just', 'an', 'beyond', 'amongst', 'none', "'ve", 'say', 'via', 'but', 'often', 're', 'our', 
-        'because', 'rather', 'using', 'without', 'throughout', 'on', 'she', 'never', 'eight', 'no', 'hereupon', 
-        'them', 'whereafter', 'quite', 'which', 'move', 'thru', 'until', 'afterwards', 'fifty', 'i', 'itself', 'nâ€˜t',
-        'him', 'could', 'front', 'within', 'â€˜re', 'back', 'such', 'already', 'several', 'side', 'whence', 'me', 
-        'same', 'were', 'it', 'every', 'third', 'together'
+        'where', 'done', 'if', 'before', 'll', 'very', 'keep', 'something',
+        'nothing', 'thereupon', 'may', 'why', 'â€™s', 'therefore', 'you',
+        'with', 'towards', 'make', 'really', 'few', 'former', 'during', 'mine',
+        'do', 'would', 'of', 'off', 'six', 'yourself', 'becoming', 'through', 
+        'seeming', 'hence', 'us', 'anywhere', 'regarding', 'whole', 'down', 'seem',
+        'whereas', 'to', 'their', 'various', 'thereafter', 'â€˜d', 'above', 'put',
+        'sometime', 'moreover', 'whoever', 'although', 'at', 'four', 'each', 'among',
+        'whatever', 'any', 'anyhow', 'herein', 'become', 'last', 'between', 'still', 
+        'was', 'almost', 'twelve', 'used', 'who', 'go', 'not', 'enough', 'well', 'â€™ve',
+        'might', 'see', 'whose', 'everywhere', 'yourselves', 'across', 'myself', 'further',
+        'did', 'then', 'is', 'except', 'up', 'take', 'became', 'however', 'many', 'thence', 
+        'onto', 'â€˜m', 'my', 'own', 'must', 'wherein', 'elsewhere', 'behind', 
+        'becomes', 'alone', 'due', 'being', 'neither', 'a', 'over', 'beside', 'fifteen',
+        'meanwhile', 'upon', 'next', 'forty', 'what', 'less', 'and', 'please', 'toward',
+        'about', 'below', 'hereafter', 'whether', 'yet', 'nor', 'against', 'whereupon',
+        'top', 'first', 'three', 'show', 'per', 'five', 'two', 'ourselves', 'whenever', 
+        'get', 'thereby', 'noone', 'had', 'now', 'everyone', 'everything', 'nowhere', 'ca',
+        'though', 'least', 'so', 'both', 'otherwise', 'whereby', 'unless', 'somewhere',
+        'give', 'formerly', 'â€™d', 'under', 'while', 'empty', 'doing', 'besides', 'thus',
+        'this', 'anyone', 'its', 'after', 'bottom', 'call', 'nâ€™t', 'name', 'even', 'eleven',
+        'by', 'from', 'when', 'or', 'anyway', 'how', 'the', 'all', 'much', 'another', 'since',
+        'hundred', 'serious', 'â€˜ve', 'ever', 'out', 'full', 'themselves', 
+        'been', 'in', "'d", 'wherever', 'part', 'someone', 'therein', 'can', 'seemed',
+        'hereby', 'others', "'s", "'re", 'most', 'one', "n't", 'into', 'some', 'will',
+        'these', 'twenty', 'here', 'as', 'nobody', 'also', 'along', 'than', 'anything', 'he',
+        'there', 'does', 'we', 'â€™ll', 'latterly', 'are', 'ten', 'hers', 'should', 'they',
+        'â€˜s', 'either', 'am', 'be', 'perhaps', 'â€™re', 'only', 'namely', 'sixty', 
+        'made', "'m", 'always', 'those', 'have', 'again', 'her', 'once', 'ours', 'herself',
+        'else', 'has', 'nine', 'more', 'sometimes', 'your', 'yours', 'that', 'around',
+        'his', 'indeed', 'mostly', 'cannot', 'â€˜ll', 'too', 'seems', 'â€™m', 'himself',
+        'latter', 'whither', 'amount', 'other', 'nevertheless', 'whom', 'for', 'somehow', 
+        'beforehand', 'just', 'an', 'beyond', 'amongst', 'none', "'ve", 'say', 'via', 'but',
+        'often', 're', 'our', 'because', 'rather', 'using', 'without', 'throughout', 'on',
+        'she', 'never', 'eight', 'no', 'hereupon', 'them', 'whereafter', 'quite', 'which',
+        'move', 'thru', 'until', 'afterwards', 'fifty', 'i', 'itself', 'nâ€˜t',
+        'him', 'could', 'front', 'within', 'â€˜re', 'back', 'such', 'already', 'several',
+        'side', 'whence', 'me', 'same', 'were', 'it', 'every', 'third', 'together'
     ]
 }
 
@@ -89,54 +97,95 @@ def dictionary_of_metrics(items):
     Return:
         dictionary of metrics
     """
-    
-    mean = np.mean(items).round(2) # calculating mean
-    median = np.median(items).round(2) #calculating median
-    std = np.std(items, ddof=1).round(2) #calculating standard deviation
-    var = np.var(items, ddof=1).round(2) #calculating variance
-    min = np.min(items).round(2) # calculating minimum of value
-    max = np.max(items).round(2) # calculating maximum value
+    # calculating mean
+    mean = np.mean(items).round(2)
+    # calculating median
+    median = np.median(items).round(2)
+    # calculating standard deviation
+    std = np.std(items, ddof=1).round(2)
+    # calculating variance
+    var = np.var(items, ddof=1).round(2)
+    # calculating minimum of value
+    min = np.min(items).round(2)
+    # calculating maximum value
+    max = np.max(items).round(2)
  
-    
-    dict = {'mean':mean, 'median':median, 'std':std, 'var':var, 'min':min, 'max':max, } # dictionary of metrics
-    
-    
-    return  dict # returning dictionary of metrics named dict
-
+    dict = {'mean': mean, 'median': median,
+            'std': std, 'var': var,
+            'min': min, 'max': max
+            } # dictionary of metrics
+    # returning dictionary of metrics named dict
+    return  dict
 ### END FUNCTION
 
-
-
-#Function_2
+# Function_2
 ### START FUNCTION
 def five_num_summary(items):
     # your code here
-    return
+    """
+    The function takes a list as input.
+    The function return a dict with keys 'max', 'median', 'min', 'q1', and 'q3'
+    corresponding to the maximum, median, minimum, first quartile and third quartile,
+    respectively. All numerical values should be rounded to two decimal places.
 
+    df: The function takes a list as input
+    
+    body:
+        The function return a dict with keys 'max', 
+        'median', 'min', 'q1', and 'q3'
+        corresponding to the maximum, median, minimum, first quartile 
+        and third quartile, respectively. You may use numpy functions to 
+        aid in your calculations.
+    
+    return:
+        five_number_summary
+    """
+    five_num_sum = np.percentile(items,[0, 25, 50, 75, 100])
+    dict = {'max': five_num_sum[4], 'Median':five_num_sum[2],
+            'Min': five_num_sum[0],'Q1':five_num_sum[1],
+            'Q3':five_num_sum[3]
+            }
+
+    return dict
 ### END FUNCTION
 
-
-#Function_3
+# Function_3
 ### START FUNCTION
+"""This code serves to shorten input list which is given as 
+    year-month-date:minutes and seconds to year-month-date
+
+    df: 
+        function takes list of strings as input.
+    
+    body:
+        this function takes as input a list of these 
+        datetime strings.
+        
+    return:
+        returns a list of strings where each element in the
+        returned list contains only the date in the 'yyyy-mm-dd'
+    """
 def date_parser(dates):
     # your code here
-    return
+    #This portion of the code slices the date to the required length.
+    new_list = [magic[0:10] for magic in dates]
+    # We return the list with sliced dates.
+    return(new_list)
 ### END FUNCTION
 
-
-
-#Function_4
-
+# Function_4
 ### START FUNCTION
 def extract_municipality_hashtags(df):
-    """ Function which takes in a pandas dataframe and returns a modified dataframe that includes 
-    two new columns that contain information about the municipality and hashtag of the tweet 
+    """ Function which takes in a pandas dataframe and returns a 
+    modified dataframe that includes two new columns that contain 
+    information about the municipality and hashtag of the tweet. 
 
-    Param
+    Param:
         Pandas dataframe df
 
-    Body
-        Modifies the dataFrame to extract the municipality and hashtags. Assigns null value to missing data.
+    Body:
+        Modifies the dataFrame to extract the municipality and hashtags. 
+        Assigns null value to missing data.
     
     Return:
         Modified dataframe with Municipality and Hashtags column
@@ -151,8 +200,13 @@ def extract_municipality_hashtags(df):
     new_tag_list = []
 
     for tweet in tweets:
-        municipality.append([mun_dict[word] for word in tweet if word in list(mun_dict.keys())])
-        hashtags.append([tag.lower() for tag in tweet if tag.startswith('#')])
+        municipality.append([mun_dict[word] 
+                            for word in tweet 
+                            if word in list(mun_dict.keys())
+                            ])
+        hashtags.append([tag.lower() for tag in tweet 
+                        if tag.startswith('#')]
+                        )
 
     for item in municipality:
         if item == []:
@@ -168,8 +222,6 @@ def extract_municipality_hashtags(df):
     df['hashtags'] = new_tag_list
   
     return df
-     
-
 ### END FUNCTION
 
 
@@ -177,9 +229,29 @@ def extract_municipality_hashtags(df):
 #Function_5
 
 ### START FUNCTION
+""""
+    This code serves to calculate the number of tweets per day, 
+    taking in a pandas dataframe as input and returning a new 
+    dataframe grouped by day and number of tweets 
+    parameter: 
+        df: function takes a pandas dataframe as input.
+    
+    body:
+        function calculates the number of tweets that were posted per day.
+    
+    return:
+        function returns a new dataframe, grouped by day, with the number
+        of tweets for that day..
+"""
 def number_of_tweets_per_day(df):
     # your code here
-    return
+    # We slice the portion of the code needed.
+    df['Date'] = [date[0:10] for date in df['Date']]
+    # Drop the duplicated column.
+    new_df = df.drop(columns=['Date'],axis=1)
+    # Group the contents of the column by the sliced date.
+    new_df = new_df.groupby(df['Date']).count() 
+    return new_df
 
 ### END FUNCTION
 
@@ -190,25 +262,33 @@ def number_of_tweets_per_day(df):
 ### START FUNCTION
 def word_splitter(df):
     # your code here
-    """eter: 
-        df: expected parameter is a pandas dataframe
+    """
+        parameter: 
+        
+        df: 
+            expected parameter is a pandas dataframe
     
-    body:  function that splits the sentences ina dataframe into a list
-    of the separate words and returns a modified dataFrame
+    body: 
+        function that splits the sentences ina dataframe into a list
+        of the separate words and returns a modified dataFrame
 
-    Param
-        modifies the dataFrame into split words and assigns them to splits tweet
+    Param:
+        modifies the dataFrame into split words and assigns them to 
+        splits tweet
     
     Return:
         modified dataframe with a new column of split tweets
     """
-    new_df = pd.DataFrame(df) # creating a new dataframe from the old dataframe
-    column1 = df['Tweets'].str.lower().values.tolist() #tweets column is extracted and coverted to a list 
-    new_list = [i.split() for i in column1] # spliting the tweets
-    new_df['Split Tweets'] = new_list # assigning new_list to split tweets column
-    
-    return new_df # returning the modified dataframe named new_df
-
+    # creating a new dataframe from the old dataframe
+    new_df = pd.DataFrame(df)
+    # tweets column is extracted and coverted to a list 
+    column1 = df['Tweets'].str.lower().values.tolist()
+    # spliting the tweets 
+    new_list = [i.split() for i in column1]
+    # assigning new_list to split tweets column 
+    new_df['Split Tweets'] = new_list 
+    # returning the modified dataframe named new_df
+    return new_df 
 ### END FUNCTION
 
 
@@ -216,19 +296,22 @@ def word_splitter(df):
 #Function_7
 ### START FUNCTION
 def stop_words_remover(df):
-   """Function that splits the sentences ina dataframe into a list
+    """
+    Function that splits the sentences ina dataframe into a list
     of the separate words and removes all stop words 
 
-     Param
+     Param:
         Pandas dataframe df
         
-    Body
-        Modifies the dataFrame into split words and removes the stop words from the tweet
+    Body:
+        Modifies the dataFrame into split words and 
+        removes the stop words from the tweet
     
     Return:
         Modified dataframe of split tweets without stop words
     """
-    df['Without Stop Words'] = df['Tweets'].apply(lambda x: [item for item in str(x).lower().split() if item not in stop_words_dict['stopwords']])
+    df['Without Stop Words'] = df['Tweets'].apply(lambda x: [item for item in str(x).lower().split() 
+                                                  if item not in stop_words_dict['stopwords']])
 
     return df
 
